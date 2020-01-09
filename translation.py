7318f8b03b0933969db189ea570a29e275446ab0
Brais Muñiz <mc.brais@gmail.com>
2020-01-09 12:15:36 +0100

    Creates translation.py module

---
+++ translation.py
@@ -0,0 +1,217 @@
+import clingo
+import re
+from more_itertools import unique_everseen
+from clingo_utilities import body_variables, add_prefix
+
+
+class XClingoProgramControl(clingo.Control):
+    """
+    Extends clingo.Control class with xclingo functions and parameters.
+    """
+    _rule_counter = None
+    traces = None
+    have_explain = None
+
+    def __init__(self, *args):
+        self.rule_counter = 0
+        self.traces = {}
+        self.have_explain = False
+        super().__init__(*args)
+
+    def count_rule(self):
+        current = self.rule_counter
+        self.rule_counter += 1
+        return current
+
+
+def _translate_label_rules(program):
+    """
+    Replaces the 'label_rule' magic comments in the given program for a version of the rules labelled with theory atoms.
+    @param str program: the program that is intended to be modified.
+    @return str:
+    """
+    for hit in re.findall("(%!label_rule \{(.*)\}[ ]*\n[ ]*([a-z][a-zA-Z]*(\(.*\))?[ ]*:-[ ]*.*).)", program):
+        # 0: original match  1: label_parameters  2: complete original rule  3: parameters of the head (useless)
+        program = program.replace(
+            hit[0],
+            "{rule}, &label_rule{{{label_parameters}}}.\n".format(rule=hit[2], label_parameters=hit[1])
+        )
+
+    return program
+
+
+def _translate_label_atoms(program):
+    """
+    Replaces the 'label_atoms' magic comments in the given program for label_atoms rule.
+    @param str program: the program that is intended to be modified
+    @return str:
+    """
+    for hit in re.findall("(%!label_atoms \{(.*)\} (([a-z][a-zA-Z]*)(\(.*\))?([ ]*:-[ ]*(.*))?.))", program):
+        # 0: original match 1: "label",v1,v2  2: complete rule  3: name  4: arguments  5: separator and body  6: body.
+        program = program.replace(
+            hit[0],
+            "&label_atoms{{{name}{arguments},{parameters} : }} :- {name}{arguments}{rest_body}.\n".format(
+                name=hit[3], arguments=hit[4], parameters=hit[1], rest_body="," + hit[6] if hit[6] else "")
+        )
+
+    return program
+
+
+def _translate_explains(program):
+    """
+    Replaces 'explain' magic comments in the given program for a rule version of those magic comments.
+    @param str program:
+    @return:
+    """
+    for hit in re.findall("(%!explain (([a-z][a-zA-Z]*(\(.*\))?)([ ]*:-[ ]*(.*))?.))", program):
+        # 0: original match  1: rule  2: head of the rule  3: parameters of the head  4: body with :-  5: body without :-
+        program = program.replace(
+            hit[0],
+            "{prefix}{head}:-{head}{body}.".format(prefix="explain_", head=hit[2], body="," + hit[5] if hit[5] else "")
+        )
+
+    return program
+
+
+def _handle_body_labels(body_asts):
+    """
+    Divides the given body (list of clingo.ast) into label theory atoms, normal literals and the rest.
+    @param List[clingo.ast.AST] body_asts:
+    @return (label_body, literals, other): tuple containing the list of label theory atoms, the symbolic atoms and
+    the rest (respectively).
+    """
+    label_body = []
+    literals = []
+    other = []
+
+    for ast in body_asts:
+        if ast['atom'].type == clingo.ast.ASTType.TheoryAtom:
+            label_body.append(ast)
+        elif ast['atom'].type == clingo.ast.ASTType.SymbolicAtom:
+            literals.append(ast)
+        else:
+            other.append(ast)
+
+    return label_body, literals, other
+
+
+def _add_to_base(rules_to_add, builder, t_option):
+    """
+    @param str rules_to_add: the rules that will be added to the base program.
+    @param clingo.ProgramBuilder builder: builder of the clingo control object that will receive the generated rules.
+    @param bool t_option: if true, the rules will not be added to the program. They will be printed by stdout instead.
+    @return None:
+    """
+    if t_option:
+        print(rules_to_add)
+        return
+
+    # Adds the generated rules to the base program
+    try:
+        clingo.parse_program("#program base." + rules_to_add, lambda new_ast: builder.add(new_ast))
+    except RuntimeError as error:
+        if str(error) == "syntax error":
+            print("Translation error:\n\n{0}".format(rules_to_add))
+            exit(0)
+
+
+def _translate_to_fired_holds(ast, control, builder, t_option):
+    """
+    Generates the label rules (useless for now), the fired rules and the holds rules for the given ast rule and adds
+    them to the 'base' program via the given builder.
+
+    @param clingo.ast.AST ast: the AST from which the rules will be generated
+    @param clingo.ProgramBuilder builder: builder of the clingo control object that will receive the generated rules.
+    @param bool t_option:
+    @return: None
+    """
+    if ast.type == clingo.ast.ASTType.Rule:
+
+        if (ast['head'].type == clingo.ast.ASTType.TheoryAtom and ast['head']['term']['name'] == "label_atoms")\
+                or (str(ast['head']).startswith("explain_")):
+            if ast['body']:
+                translated_rule = "{head} :- {translated_body}.".format(
+                    head=str(ast['head']),
+                    translated_body=",".join([str(add_prefix('holds_', lit)) for lit in ast['body']])
+                )
+            else:
+                translated_rule = str(ast['head']) + "."
+
+            generated_rules = translated_rule + "\n"
+        else:
+            rule_counter = control.count_rule()
+
+            # Separates the &label literals in the body from the rest
+            label_body, literals, other = _handle_body_labels(ast['body'])
+
+            # Associates fired number and function name
+            control.traces[rule_counter] = {
+                'head': (str(ast['head']['atom']['term']['name']), ast['head']['atom']['term']['arguments']),
+                'arguments': list(unique_everseen(map(str, ast['head']['atom']['term']['arguments'] + body_variables(ast['body'])))),
+                'body': [(lit['atom']['term']['name'], lit['atom']['term']['arguments']) for lit in literals if lit['sign'] == clingo.ast.Sign.NoSign]
+            }
+
+            if ast['head'].type == clingo.ast.ASTType.TheoryAtom:  # For label rules
+                generated_rules = "%{comment}\n{head} :- {body}.\n".\
+                    format(comment=str(ast),
+                           head=str(ast['head']),
+                           body=",".join([str(add_prefix('holds_', lit)) for lit in literals]))
+            else:
+                # Generates fired rule
+                fired_head = "fired_{counter}({arguments})".format(
+                    counter=str(rule_counter),
+                    arguments=",".join(unique_everseen(map(str, ast['head']['atom']['term']['arguments'] + body_variables(ast['body']))))
+                )
+                if literals or other:
+                    fired_rule = "{head} :- {body}.".format(
+                        head=fired_head,
+                        body=",".join([str(add_prefix('holds_', lit)) for lit in literals] + list(map(str, other))))
+                else:
+                    fired_rule = fired_head + "."
+
+                # Generates holds rule
+                holds_rule = "holds_{name}({arguments}) :- {body}.".\
+                    format(name=str(ast['head']['atom']['term']['name']),
+                           arguments=",".join(map(str, ast['head']['atom']['term']['arguments'])),
+                           body=fired_head)
+
+                # Generates label rules
+                label_rules = ""
+                for label_ast in label_body:
+                    label_rules += "&label_rule{{{fired_id},{label_parameters}}} :- {body}.\n".format(
+                        fired_id=str(rule_counter),
+                        label_parameters=",".join([str(e) for e in label_ast['atom']['elements']]),
+                        body=fired_head)
+
+                # Generates a comment
+                comment = "%" + str(ast)
+
+                generated_rules = comment + "\n" + fired_rule + "\n" + holds_rule + "\n" + label_rules
+
+        _add_to_base(generated_rules, builder, t_option)
+
+
+def prepare_xclingo_program(original_program, t_option):
+    control = XClingoProgramControl(["-n 0", "--keep-facts"])
+
+    # Pre-processing original program
+    translated_program = _translate_label_rules(original_program)
+    translated_program = _translate_label_atoms(translated_program)
+
+    aux = translated_program
+    translated_program = _translate_explains(translated_program)
+
+    control.have_explain = bool(aux != translated_program)
+
+    # Sets theory atom &label and parses/handles input program
+    with control.builder() as builder:
+        # Adds theories
+        clingo.parse_program("#program base. #theory label_rule {t { }; &label_rule/0: t, any}.",
+                             lambda ast: builder.add(ast))
+        clingo.parse_program("#program base. #theory label_atoms {t { }; &label_atoms/0: t, any}.",
+                             lambda ast: builder.add(ast))
+        # Handle xclingo sentences
+        clingo.parse_program("#program base." + translated_program,
+                             lambda ast: _translate_to_fired_holds(ast, control, builder, t_option))
+
+    return control
--- xclingo.py
+++ xclingo.py
@@ -1,203 +1,12 @@
 
-import re
+
 import sys
 import argparse
 import clingo
 
 from pandas import DataFrame
-from itertools import chain
-from more_itertools import unique_everseen
-from clingo_utilities import find_by_prefix, body_variables, add_prefix, remove_prefix, find_and_remove_by_prefix
-
-rule_counter = 0
-traces = {}
-
-
-def preprocess_program(program):
-    """
-    Performs a set of different process phases to a given program and return the modified result. This phases are:
-     - label_rule magic comments translation.
-    @param str program: the program that is intended to be process.
-    @return str: the modification of the given program after the different phases.
-    """
-    preprocessed_program = translate_label_rules(program)
-
-    return preprocessed_program
-
-
-def translate_label_rules(program):
-    """
-    Replaces the 'label_rule' magic comments in the given program for a version of the rules labelled with theory atoms.
-    @param str program: the program that is intended to be modified.
-    @return str: the modified program.
-    """
-    for hit in re.findall("(%!label_rule \{(.*)\}[ ]*\n[ ]*([a-z][a-zA-Z]*(\(.*\))?[ ]*:-[ ]*.*).)", program):
-        # 0: original match  1: label_parameters  2: complete original rule  3: parameters of the head (useless)
-        program = program.replace(
-            hit[0],
-            "{rule}, &label_rule{{{label_parameters}}}.\n".format(rule=hit[2], label_parameters=hit[1])
-        )
-
-    return program
-
-
-def get_multiple_label_rules(program):
-    """
-    Returns a list with the multiple label sentences found in the given program.
-    @param str program:
-    @return:
-    """
-    multiple_label_rules = ""
-    for hit in re.findall("%!label_atoms \{(.*)\} (([a-z][a-zA-Z]*)(\(.*\))?([ ]*:-[ ]*(.*))?.)", program):
-        # 0: "label",v1,v2  1: complete rule  2: name  3: arguments  4: separator and body  5: body.
-        multiple_label_rules += "&label_atoms{{{name}{arguments},{parameters} : }} :- {name}{arguments}{rest_body}.\n".\
-            format(name=hit[2], arguments=hit[3], parameters=hit[0], rest_body="," + hit[5] if hit[5] else "")
-    return multiple_label_rules
-
-
-def get_explain_sentences(program):
-    """
-    Returns a list with the explain sentences found in the given program.
-    @param str program:
-    @return:
-    """
-    explain_sentences_string = ""
-    for hit in re.findall("%!explain ([a-z][a-zA-Z]*(\(.*\))?([ ]*:-[ ]*.*)?.)", program):
-        explain_sentences_string += str(hit[0])
-
-    return explain_sentences_string
-
-
-def handle_body_labels(body_asts):
-    """
-    @param List[clingo.ast.AST] body_asts:
-    @return (label_body, literals, other): tuple containing the list of label theory atoms, the symbolic atoms and
-    the rest (respectively).
-    """
-    label_body = []
-    literals = []
-    other = []
-
-    for ast in body_asts:
-        if ast['atom'].type == clingo.ast.ASTType.TheoryAtom:
-            label_body.append(ast)
-        elif ast['atom'].type == clingo.ast.ASTType.SymbolicAtom:
-            literals.append(ast)
-        else:
-            other.append(ast)
-
-    return label_body, literals, other
-
-
-def add_to_base(generated_rules, builder, t_option):
-    """
-    @param str generated_rules: the rules that will be added to the base program.
-    @param clingo.ProgramBuilder builder: builder of the clingo control object that will receive the generated rules.
-    @param bool t_option: if true, the rules will not be added to the program. They will be printed by stdout instead.
-    @return None:
-    """
-    if t_option:
-        print(generated_rules)
-        return
-
-    # Adds the generated rules to the base program
-    try:
-        clingo.parse_program("#program base." + generated_rules, lambda new_ast: builder.add(new_ast))
-    except RuntimeError as error:
-        if str(error) == "syntax error":
-            print("Translation error:\n\n{0}".format(generated_rules))
-            exit(0)
-
-
-def generate_label_atoms_rules(ast, builder, t_option):
-    # Generate the multiple label rule for the given ast and adds it to the base program
-    if ast.type == clingo.ast.ASTType.Rule:
-        generated_rule = "{head}:-{body}.".format(
-            head=str(ast['head']),
-            body=",".join([str(add_prefix("holds_", lit)) for lit in ast['body']]))
-
-        add_to_base(generated_rule, builder, t_option)
-
-
-def generate_explain_rules(ast, builder, t_option):
-    # Generates the explain rule for the given ast and adds it to the base program.
-    if ast.type == clingo.ast.ASTType.Rule:
-        # starts with explain
-        generated_rules = "explain_{head}:-{body}.".format(
-            head=str(ast['head']),
-            body=",".join([str(add_prefix("holds_", lit)) for lit in [ast['head']] + ast['body']]))
-
-        add_to_base(generated_rules, builder, t_option)
-
-
-def generate_fired_holds_rules(ast, builder, t_option):
-    """
-    Generates the label rules (useless for now), the fired rules and the holds rules for the given ast rule and adds
-    them to the 'base' program via the given builder.
-
-    @todo: individual label rules must be generated from 'magic comments' and not from theory atoms.
-
-    @param clingo.ast.AST ast: the AST from which the rules will be generated
-    @param clingo.ProgramBuilder builder: builder of the clingo control object that will receive the generated rules.
-    @param bool t_option:
-    @return: None
-    """
-    global rule_counter
-    rule_counter += 1
-
-    if ast.type == clingo.ast.ASTType.Rule:
-
-        if str(ast['head']).startswith("explain_"):
-            generated_rules = str(ast)
-        else:
-            # Separates the &label literals in the body from the rest
-            label_body, literals, other = handle_body_labels(ast['body'])
-
-            # Associates fired number and function name
-            global traces
-            traces[rule_counter] = {
-                'head': (str(ast['head']['atom']['term']['name']), ast['head']['atom']['term']['arguments']),
-                'arguments': list(unique_everseen(map(str, ast['head']['atom']['term']['arguments'] + body_variables(ast['body'])))),
-                'body': [(lit['atom']['term']['name'], lit['atom']['term']['arguments']) for lit in literals if lit['sign'] == clingo.ast.Sign.NoSign]
-            }
-
-            if ast['head'].type == clingo.ast.ASTType.TheoryAtom:  # For label rules
-                generated_rules = "%{comment}\n{head} :- {body}.\n".\
-                    format(comment=str(ast),
-                           head=str(ast['head']),
-                           body=",".join([str(add_prefix('holds_', lit)) for lit in literals]))
-            else:
-                # Generates fired rule
-                fired_head = "fired_{counter}({arguments})".format(
-                    counter=str(rule_counter),
-                    arguments=",".join(unique_everseen(map(str, ast['head']['atom']['term']['arguments'] + body_variables(ast['body'])))))
-                if literals or other:
-                    fired_rule = "{head} :- {body}.".format(
-                        head=fired_head,
-                        body=",".join([str(add_prefix('holds_', lit)) for lit in literals] + list(map(str, other))))
-                else:
-                    fired_rule = fired_head + "."
-
-                # Generates holds rule
-                holds_rule = "holds_{name}({arguments}) :- {body}.".\
-                    format(name=str(ast['head']['atom']['term']['name']),
-                           arguments=",".join(map(str, ast['head']['atom']['term']['arguments'])),
-                           body=fired_head)
-
-                # Generates label rules
-                label_rules = ""
-                for label_ast in label_body:
-                    label_rules += "&label_rule{{{fired_id},{label_parameters}}} :- {body}.\n".format(
-                        fired_id=str(rule_counter),
-                        label_parameters=",".join([str(e) for e in label_ast['atom']['elements']]),
-                        body=fired_head)
-
-                # Generates a comment
-                comment = "%" + str(ast)
-
-                generated_rules = comment + "\n" + fired_rule + "\n" + holds_rule + "\n" + label_rules
-
-        add_to_base(generated_rules, builder, t_option)
+from clingo_utilities import find_by_prefix, remove_prefix, find_and_remove_by_prefix
+import translation
 
 
 def build_fired_dict(m):
@@ -373,23 +182,10 @@ def main():
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="ASP program")
     args = parser.parse_args()
 
-    # Gets clingo control object (with '-n 0')
-    control = clingo.Control(["-n 0", "--keep-facts"])
-
     original_program = args.infile.read()
 
-    # Pre-processing original program
-    explain_sentences = get_explain_sentences(original_program)
-    label_atoms_sentences = get_multiple_label_rules(original_program)
-    preprocessed_program = preprocess_program(original_program)
-
-    # Sets theory atom &label and parses/handles input program
-    with control.builder() as builder:
-        clingo.parse_program("#program base. #theory label_rule {t { }; &label_rule/0: t, any}.", lambda ast: builder.add(ast))
-        clingo.parse_program("#program base. #theory label_atoms {t { }; &label_atoms/0: t, any}.", lambda ast: builder.add(ast))
-        clingo.parse_program("#program base." + explain_sentences, lambda ast: generate_explain_rules(ast, builder, args.t))
-        clingo.parse_program("#program base." + label_atoms_sentences, lambda ast: generate_label_atoms_rules(ast, builder, args.t))
-        clingo.parse_program("#program base." + preprocessed_program, lambda ast: generate_fired_holds_rules(ast, builder, args.t))
+    # Prepare the original program and obtain XClingoControl
+    control = translation.prepare_xclingo_program(original_program, args.t)
 
     # JUST FOR DEBUGGING TODO: delete this
     if args.t:
@@ -407,11 +203,11 @@ def main():
             print("Answer: " + str(sol_n))
 
             fired_explains = [remove_prefix("explain_", a) for a in find_and_remove_by_prefix(m, 'explain_')]
-            causes = build_causes(traces, build_fired_dict(m), labels_dict, args.auto_labelling)
+            causes = build_causes(control.traces, build_fired_dict(m), labels_dict, args.auto_labelling)
 
-            if explain_sentences and fired_explains:
+            if control.have_explain and fired_explains:
                 atoms_to_explain = [a for a in causes['fired_head'].unique() if a in fired_explains]
-            elif explain_sentences and not fired_explains:
+            elif control.have_explain and not fired_explains:
                 print("Any explain rule was activated.")
                 atoms_to_explain = []
             else:
