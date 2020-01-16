21345896399e3c259ebd0d6ff900b4a8e63314b6
Brais Muñiz <mc.brais@gmail.com>
2020-01-16 11:14:59 +0100

    Modifies clingo XClingoAST class to use composite

--- translation.py
+++ translation.py
@@ -1,3 +1,5 @@
+from collections import Iterable
+
 from clingo import Control, parse_program, ast
 import re
 from more_itertools import unique_everseen
@@ -24,19 +26,98 @@ class XClingoProgramControl(Control):
         return current
 
 
-class XClingoAST(ast.AST):
+class XClingoAST():
+    _internal_ast = None  # Type: ast.AST
+    type = None  # Type: ast.AST.ASTType
+    child_keys = None  # Type: List [ str ]
+
+    # l =[method for method in dir(my_ast) if method != 'use_enumeration_assumption' and callable(getattr(my_ast, method))]
 
     def __init__(self, base_ast):
-        super().__init__(base_ast.type)
-        # Convert children that are AST
-        for k in base_ast:
-            if type(base_ast[k]) == ast.AST:
-                self[k] = XClingoAST(base_ast[k])
-            elif type(base_ast[k]) == list:  # Only the elements that are AST
-                self[k] = [XClingoAST(item) if type(item) == ast.AST else item
-                           for item in base_ast[k]]
-            else:  # If is not AST or list(AST) then do nothing
-                self[k] = base_ast[k]
+        """
+
+        @param ast.AST base_ast:
+        """
+        self._internal_ast = base_ast
+
+        self.type = base_ast.type
+        self.child_keys = base_ast.child_keys
+
+    """ast.AST methods"""
+    def items(self):
+        return self._internal_ast.items()
+
+    def keys(self):
+        return self._internal_ast.keys()
+
+    def values(self):
+        return self._internal_ast.values()
+
+    """container methods and other"""
+    def __contains__(self, item):
+        return self._internal_ast.__contains__(item)
+
+    def __getitem__(self, item):
+        item = self._internal_ast.__getitem__(item)
+        if type(item) == ast.AST:
+            return XClingoAST(item)
+        if type(item) == list:
+            new_list = []
+            for i in item:
+                if type(i) == ast.AST:
+                    new_list.append(XClingoAST(i))
+                else:
+                    new_list.append(i)
+            return new_list
+        else:
+            return item
+
+    def __delattr__(self, item):
+        self._internal_ast.__delattr__(item)
+
+    def __delitem__(self, key):
+        self._internal_ast.__delitem__(key)
+
+    def __eq__(self, other):
+        return self._internal_ast.__eq__(other)
+
+    def __format__(self, format_spec):
+        return self._internal_ast.__format__(format_spec)
+
+    def __ge__(self, other):
+        return self._internal_ast.__ge__(other)
+
+    # def __getattribute__(self, item):
+    #     return self._internal_ast.__getattribute__(item)
+
+    def __gt__(self, other):
+        return self._internal_ast.__gt__(other)
+
+    def __iter__(self):
+        return self._internal_ast.__iter__()
+
+    def __le__(self, other):
+        return self._internal_ast.__le__()
+
+    def __len__(self):
+        return self._internal_ast.__len__()
+
+    def __lt__(self, other):
+        return self._internal_ast.__lt__()
+
+    def __ne__(self, other):
+        return self._internal_ast.__ne__()
+
+    # def __setattr__(self, key, value):
+    #     self._internal_ast.__setattr__(key, value)
+
+    def __setitem__(self, key, value):
+        self._internal_ast.__setitem__(key, value)
+
+    def __str__(self):
+        return self._internal_ast.__str__()
+
+    """XclingoAST methods"""
 
     def is_constraint(self):
         """
@@ -98,7 +179,7 @@ class XClingoAST(ast.AST):
             return self['argument'].get_function()
 
         if self.type == ast.ASTType.Literal:
-            return XClingoAST(self['atom']).get_function()
+            return self['atom'].get_function()
 
         if self.type in (ast.ASTType.Comparison, ast.ASTType.BooleanConstant):
             return None
